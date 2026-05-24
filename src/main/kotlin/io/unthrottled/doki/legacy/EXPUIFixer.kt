823eb7e716d23dd9f5cc9886a52ef8e83c016488
Arimil <renari@arimil.com>
2026-05-24 03:46:20 -0400

    don't enforce tree line height

--- src/main/kotlin/io/unthrottled/doki/legacy/EXPUIFixer.kt
+++ src/main/kotlin/io/unthrottled/doki/legacy/EXPUIFixer.kt
@@ -34,7 +34,6 @@ object EXPUIFixer : LafManagerListener, Disposable {
         setUIProperty("ToolWindow.Button.selectedBackground", dokiTheme.getColor("highlightColor"), defaults)
         setUIProperty("ToolWindow.Button.selectedForeground", dokiTheme.getColor("iconAccent"), defaults)
         setUIProperty("Editor.SearchField.borderInsets", JBUI.insets(7, 10, 7, 8), defaults)
-        setUIProperty("Tree.rowHeight", 24, defaults)
         setUIProperty("Tree.border", JBUI.insets(4, 12), defaults)
       }
       overrideSetProperties(iterations + 1)
